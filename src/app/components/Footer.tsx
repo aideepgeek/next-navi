a869e7556779187546da8b21ac512c346ec0c8ea
ibillxia <aideepgeek@gmail.com>
2025-09-06 23:28:53 +0800

    first commit
    1st commit

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import React from 'react'
+
+type Props = {}
+
+const Footer = (props: Props) => {
+  return (
+    <div className='h-screen w-full flex flex-col items-center justify-end'>
+        <p className='text-slate-500 mb-4'>Copyright © 2025 </p>
+    </div>
+  )
+}
+
+export default Footer
